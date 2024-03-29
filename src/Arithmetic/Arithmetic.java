3edb1452e1f6a28355ac9f88a22837025ef77f91
moriel <morielbien101@gmail.com>
2024-03-29 16:40:26 +0800

    added Arithmetic.java functions

--- src/Arithmetic/Arithmetic.java
+++ src/Arithmetic/Arithmetic.java
@@ -1,5 +1,43 @@
 package Arithmetic;
 
 public class Arithmetic {
+    private Number num1;
+    private Number num2;
 
+    public Arithmetic(Number num1, Number num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public double add() {
+        return num1.doubleValue() + num2.doubleValue();
+    }
+
+    public double subtract() {
+        return num1.doubleValue() - num2.doubleValue();
+    }
+
+    public double multiply() {
+        return num1.doubleValue() * num2.doubleValue();
+    }
+
+    public double divide() {
+        return num1.doubleValue() / num2.doubleValue();
+    }
+
+    public Number getMin() {
+        if (num1.doubleValue() < num2.doubleValue()) {
+            return num1;
+        } else {
+            return num2;
+        }
+    }
+
+    public Number getMax() {
+        if (num1.doubleValue() > num2.doubleValue()) {
+            return num1;
+        } else {
+            return num2;
+        }
+    }
 }
