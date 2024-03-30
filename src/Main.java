5f0c5157524dce2aad15f75413440b51242fd59e
moriel <morielbien101@gmail.com>
2024-03-30 14:37:35 +0800

    added some tests to run Arithmetic.java if it works

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import Arithmetic.Arithmetic;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        // Testing Arithmetic
+        Arithmetic arithmetic = new Arithmetic(10, 5.5);
+        System.out.println("Addition: " + arithmetic.add());
+        System.out.println("Subtraction: " + arithmetic.subtract());
+        System.out.println("Multiplication: " + arithmetic.multiply());
+        System.out.println("Division: " + arithmetic.divide());
+        System.out.println("Min: " + arithmetic.getMin());
+        System.out.println("Max: " + arithmetic.getMax());
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
